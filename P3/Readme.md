a3d4f178b6b6c039e39b5cd93c15f15fb2094bb9
Jorge Rodriguez <jrodmo@Air-de-Jorge-5.home>
2020-04-22 20:00:00 +0200

    juego retro con cambios

--- P3/Readme.md
+++ P3/Readme.md
@@ -1,1 +1,2 @@
 # Práctica 3
+Pong, en el que he modificado que una pala se mueva sola, aunque existe la opcion de controlar las dos añadiendo comentario en la variable pala maquina, y quitando el comentario justo debajo en una variable también llamada así y otro cambio es que se lanzan tres bolas y una solo es la que cuenta con la puntuación.
--- P3/pong.js
+++ P3/pong.js
@@ -1,307 +1,498 @@
-var ANCHO = 600;
-var ALTURA = 400;
-var SCORE1 = 0;
-var SCORE2=0;
-
-function makeScore(ANCHO, ALTURA){
-  this.ctx = null;
-
-  this.score1 = SCORE1;
-  this.score2 = SCORE2;
-
-  this.ancho = ANCHO;
-  this.altura = ALTURA;
-
-  this.init =  function(ctx){
-    this.reset();
-    this.ctx = ctx;
-  };
-
-  this.draw = function (){
-    this.ctx.font = "70px American Typewriter";
-    this.ctx.fillStyle = 'gray';
-    //--SCORE1:
-    this.ctx.fillText(this.score1, 240,60 );
-    //--SCORE2:
-    this.ctx.fillText(this.score2, 320,60 );
-
-    //-- RED:
-    var i = 0;
-    while (i < this.altura){
-      this.ctx.fillStyle = 'white';
-      this.ctx.fillRect(300, i, 1, 10)
-      i += 15;
-    }
-  };
-  this.reset = function(){
-    this.score1 = 0;
-    this.score2 = 0;
-  }
-}
-
-function dopalas (x,y, ALTURA){
-  this.ctx = null;
-
-  this.x_ini = x;
-  this.y_ini = y;
-
-  this.ancho = 10;
-  this.altura = 50;
-
-  this.vy = 0;
-  this.speed = 5; //-- Nivel de dificultad: EASY, NORMAL, HARD
-
-  this.init = function(ctx){
-    this.reset();
-    this.ctx = ctx;
-  };
-
-  this.draw = function () {
-    this.ctx.fillStyle = 'black';
-    this.ctx.fillRect(this.x, this.y, this.ancho, this.altura);
-  };
-
-  this.reset = function (){
-    this.x = this.x_ini;
-    this.y = this.y_ini;
-  };
-
-  this.update = function() {
-    //-- Movimiento de la pala según el nivel de dificultad:
-    this.y += this.vy*this.speed;
-    //-- Para que no se salgan del canvas:
-    if (this.y > ALTURA - this.altura){
-      this.y = ALTURA - this.altura;
-    } else if (this.y < 0){
-      this.y = 0;
-    }
-  };
-}
-
-function dobola() {
-  this.ctx = null;
-
-  //-- Inicialmente saca el Jugador1:
-  this.x_ini = 85;
-  this.y_ini = 125;
-
-  this.x = 0;
-  this.y = 0;
-
-  this.ancho = 5;
-  this.altura = 5;
-
-  this.vx = 5;
-  this.vy = 2;
-  this.speed = 1; //-- Nivel de dificultad: EASY, NORMAL, HARD
-
-  this.init = function(ctx){
-    this.reset();
-    this.ctx = ctx;
-  };
-
-  this.draw = function () {
-    this.ctx.fillStyle = 'yellow';
-    this.ctx.beginPath();
-    //-- Dibujar un circulo: coordenadas x,y del centro
-    //-- Radio, Angulo inicial y angulo final
-    this.ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
-    //-- BOLA CUADRADA:
-    // this.ctx.fillRect(this.x, this.y, this.width, this.height)
-    this.ctx.fill()
-  };
-
-  this.update = function () {
-    this.x += this.vx*this.speed;
-    this.y += this.vy*this.speed;
-
-    if (this.y > ALTURA - this.altura || this.y < this.altura){
-      this.vy = -this.vy;
-    }
-  };
-
-  this.reset = function() {
-    this.x = this.x_ini;
-    this.y = this.y_ini;
-  };
-  }
-
-function moverPalas(pala1, pala2) {
-  window.onkeydown = (e) => {
-    e.preventDefault();
-    switch (e.key) {
-      case 'w':
-          pala1.vy = -1;
-          break;
-      case 's':
-          pala1.vy = 1;
-          break;
-      case 'ArrowUp':
-          pala2.vy = -1;
-          break;
-      case 'ArrowDown':
-          pala2.vy = 1;
-          break;
-      default:
-          break;
-    }
-  }
-  window.onkeyup = (e) => {
-    e.preventDefault();
-    switch (e.key) {
-      case 'w':
-          pala1.vy = 0;
-          break;
-      case 's':
-          pala1.vy = 0;
-          break;
-      case 'ArrowUp':
-          pala2.vy = 0;
-          break;
-      case 'ArrowDown':
-          pala2.vy = 0;
-          break;
-      default:
-          break;
-    }
-  }
-}
-
-//-- Función para el REBOTE de la bola:
-function pushbola(pala1, pala2, bola){
-  if (bola.x <= (pala1.x + pala1.ancho) && bola.x >= pala1.x) {
-    if (bola.y >= pala1.y && bola.y <= (pala1.y + pala1.altura)){
-      bola.vx = -bola.vx;
-    }
-  };
-
-  if ((bola.x + bola.ancho) >= pala2.x && (bola.x+ bola.ancho) <= (pala2.x + pala2.ancho)){
-    if ((bola.y + bola.altura) <= (pala2.y + pala2.altura) && (bola.y + bola.altura) >= pala2.y){
-      bola.vx = -bola.vx;
-    }
-  };
-}
-
-function restartbola(player, bola, pala1, pala2){
-  //-- Marca el Jugador1 -> saca 2:
-  if (player == 'Jugador1'){
-    bola.x_ini = 550;
-    bola.y_ini = 200;
-
-    //ball.vx = -ball.vx;
-
-  } else if (player == 'Jugador2'){
-    bola.x_ini = 50;
-    bola.y_ini = 100;
-
-    //ball.vx = -ball.vx;
-  };
-
-  bola.reset();
-  pala1.reset();
-  pala2.reset();
-  bola.draw();
-  pala1.draw();
-  pala2.draw();
-}
-
-function main(){
-
+function main()
+{
+  console.log('PONG: MAIN: Start!');
+  var random = Math.random();
   var canvas = document.getElementById('display')
-  canvas.width= ANCHO;
-  canvas.height = ALTURA;
+  canvas.width = 600;
+  canvas.height = 400;
 
   var ctx = canvas.getContext("2d");
 
-  var score = new makeScore(canvas.width, canvas.height);
+  var netdraw = function () {
+    ctx.setLineDash([10,12])
+    ctx.moveTo(300,0) //desde
+    ctx.lineTo(300,400) //hasta
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#1FF636';
+    ctx.stroke();
+  }
 
+  var make_random = function (a,b) {
+    return Math.round(Math.random()*(b-a)+parseInt(a));
+  }
+
+  var sign_random = function (a) {
+    n = Math.round(Math.random());
+    if (n == 1){
+      a = a * -1
+    }
+    return a
+  }
+
+  var bin_random = function () {
+    return Math.round(Math.random());
+  }
+
+  var sett = {
+    status: null,
+    ia: null,
+    level: null,
+  }
+
+  //--Animacion
+
+  var score = {
+
+    scor1_ini: 0,
+    scor2_ini: 0,
+
+    scor1: 0,
+    scor2: 0,
+
+    ctx: null,
+
+    reset: function () {
+      this.scor1 = this.scor1_ini;
+      this.scor2 = this.scor2_ini;
+    },
+
+    init: function () {
+      this.reset();
+      this.ctx = ctx;
+    },
+    draw: function() {
+      ctx.font = "80px Arial";
+      ctx.fillStyle = '#1FF636';
+      ctx.fillText(this.scor1,220,70);
+      ctx.fillText(this.scor2,340,70);
+    },
+  }
+
+  var racks = {
+    //-- P1:
+    x1_ini: 50,
+    y1_ini: 100,
+
+    x1: 0,
+    y1: 0,
+
+    vx: 0, //movimiento horizontal
+    vy: 0, //movimiento vertical
+
+    ctx: null,
+
+    width: 10,
+    height: 40,
+
+    reset: function() {
+      this.x1 = this.x1_ini;
+      this.y1 = this.y1_ini;
+
+      //-- Velocidad de animacion
+      this.vy1 = 0;
+    },
+
+    init: function(ctx) {
+      this.reset();
+      this.ctx = ctx;
+    },
+
+    draw: function() {
+      this.ctx.fillStyle = '#1FF636';
+      this.ctx.fillRect(this.x1, this.y1, this.width, this.height);
+    },
+
+    update: function () {
+      window.onkeydown = (e) => {
+        e.preventDefault();
+        switch (e.key) {
+          case "ArrowUp":
+            //-- Comprobacion limite superior y mover
+            if (this.y1 > 0) {
+              this.vy1 = -5;
+            }
+            break;
+          case "ArrowDown":
+            //-- Comprobacion limite inferior y mover
+            if (this.y1 < canvas.height -40) {
+              this.vy1 = 5;
+            }
+            break;
+            default:
+            break;
+          }
+        }
+      window.onkeyup = (e) => {
+        e.preventDefault();
+        switch (e.key) {
+        case "ArrowUp":
+          //-- Comprobacion limite superior y mover
+          if (this.y1 > 0) {
+            this.vy1 = 0;
+          }
+          break;
+        case "ArrowDown":
+          //-- Comprobacion limite inferior y mover
+          if (this.y1 < canvas.height -40) {
+            this.vy1 = 0;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    this.y1 = this.y1 + this.vy1;
+  }
+  }
+//PARA QUE UNA DE LAS PALAS SEA AUTOMATICA
+  var palamaquina = {
+   x_init:500,
+   y_init:100,
+   x:0,
+   y:0,
+   vy:10,
+   ctx:null,
+   anchura:10,
+   altura:40,
+   reset: function(){
+     this.x= this.x_init;
+     this.y= this.y_init;
+   },
+   init : function(ctx){
+     this.reset();
+     this.ctx= ctx;
+   },
+
+   draw : function(){
+     ctx.fillStyle = '#1FF636';
+     ctx.fillRect(this.x,this.y, this.anchura, this.altura)
+     ctx.fill()
+   },
+   update: function(){
+     this.y= bola.y-this.altura/4*0.1; //
+     console.log(random)
+   },
+ }
+// PARA CONTROLAR LAS DOS PALAS
+// var palamaquina = {
+//   //-- P1:
+//   x2_ini: 500,
+//   y2_ini: 100,
+//
+//   x2: 0,
+//   y2: 0,
+//
+//   vx: 0, //movimiento horizontal
+//   vy: 0, //movimiento vertical
+//
+//   ctx: null,
+//
+//   width: 10,
+//   height: 40,
+//
+//   reset: function() {
+//     this.x2 = this.x2_ini;
+//     this.y2 = this.y2_ini;
+//
+//     //-- Velocidad de animacion
+//     this.vy2 = 0;
+//   },
+//
+//   init: function(ctx) {
+//     this.reset();
+//     this.ctx = ctx;
+//   },
+//
+//   draw: function() {
+//     this.ctx.fillStyle = '#1FF636';
+//     this.ctx.fillRect(this.x2, this.y2, this.width, this.height);
+//   },
+//
+//   update: function () {
+//     window.onkeydown = (e) => {
+//       e.preventDefault();
+//       switch (e.key) {
+//         case "w":
+//           //-- Comprobacion limite superior y mover
+//           if (this.y2 > 0) {
+//             this.vy2 = -5;
+//           }
+//           break;
+//         case "s":
+//           //-- Comprobacion limite inferior y mover
+//           if (this.y2 < canvas.height -40) {
+//             this.vy2 = 5;
+//           }
+//           break;
+//           default:
+//           break;
+//         }
+//       }
+//     window.onkeyup = (e) => {
+//       e.preventDefault();
+//       switch (e.key) {
+//       case "w":
+//         //-- Comprobacion limite superior y mover
+//         if (this.y2 > 0) {
+//           this.vy2 = 0;
+//         }
+//         break;
+//       case "s":
+//         //-- Comprobacion limite inferior y mover
+//         if (this.y2 < canvas.height -40) {
+//           this.vy2 = 0;
+//         }
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+//   this.y2 = this.y2 + this.vy2;
+// }
+// }
+  var bola = {
+
+    x_ini: 50,
+    y_ini: 50,
+
+    x: 0,
+    y: 0,
+
+    vx: 0,
+    vy: 0,
+
+    ctx: null,
+
+    width: 5,
+    height: 5,
+
+    reset: function() {
+      //--Vel. Level
+      this.vx = make_random(2,6); //--Movimiento horizontal
+      remind = 6 - this.vx; //--Movimiento vertical
+      this.vy = sign_random(remind)
+      console.log(this.vx);
+      console.log(this.vy);
+      //--Init. Position
+      nbin = bin_random()
+      if (nbin == 1) {
+        this.x_ini = 450;
+        this.vx = -this.vx;
+      }else{
+        this.x_ini = 100;
+      }
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+
+    },
+    init: function(ctx) {
+      this.reset();
+      this.ctx = ctx;
+    },
+    draw: function() {
+      this.ctx.fillStyle = '#1FF636';
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+    update: function () {
+      this.x = this.x + this.vx;
+      this.y = this.y + this.vy;
+    },
+  }
+
+  var bola2 = {
+
+    x_ini: 0,
+    y_ini: make_random(100,300),
+
+    x: 0,
+    y: 0,
+
+    vx: 0,
+    vy: 0,
+
+    ctx: null,
+
+    width: 5,
+    height: 5,
+
+    reset: function() {
+      //--Vel. Level
+      this.vx = make_random(2,6); //--Movimiento horizontal
+      remind = 6 - this.vx; //--Movimiento vertical
+      this.vy = sign_random(remind)
+      console.log(this.vx);
+      console.log(this.vy);
+      //--Init. Position
+      nbin = bin_random()
+      if (nbin == 1) {
+        this.x_ini = 450;
+        this.vx = -this.vx;
+      }else{
+        this.x_ini = 100;
+      }
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+
+    },
+    init: function(ctx) {
+      this.reset();
+      this.ctx = ctx;
+    },
+    draw: function() {
+      this.ctx.fillStyle = '#00FAFF';
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+    update: function () {
+      this.x = this.x + this.vx;
+      this.y = this.y + this.vy;
+    },
+  }
+
+  var bola1 = {
+
+    x_ini: 0,
+    y_ini: make_random(100,300),
+
+    x: 0,
+    y: 0,
+
+    vx: 0,
+    vy: 0,
+
+    ctx: null,
+
+    width: 5,
+    height: 5,
+
+    reset: function() {
+      //--Vel. Level
+      this.vx = make_random(2,6); //--Movimiento horizontal
+      remind = 6 - this.vx; //--Movimiento vertical
+      this.vy = sign_random(remind)
+      console.log(this.vx);
+      console.log(this.vy);
+      //--Init. Position
+      nbin = bin_random()
+      if (nbin == 1) {
+        this.x_ini = 450;
+        this.vx = -this.vx;
+      }else{
+        this.x_ini = 100;
+      }
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+
+    },
+    init: function(ctx) {
+      this.reset();
+      this.ctx = ctx;
+    },
+    draw: function() {
+      this.ctx.fillStyle = '#00FAFF';
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+    update: function () {
+      this.x = this.x + this.vx;
+      this.y = this.y + this.vy;
+    },
+  }
+  netdraw();
+  racks.init(ctx);
+  racks.draw();
+  palamaquina.init(ctx);
+  palamaquina.draw();
+  bola.init(ctx);
+  bola.draw();
+  bola1.init(ctx);
+  bola1.draw();
+  bola2.init(ctx);
+  bola2.draw();
   score.init(ctx);
   score.draw();
 
-  var pala1 = new dopalas(50, 100, canvas.height);
-  var pala2 = new dopalas(550, 200, canvas.height);
-
-  pala1.init(ctx);
-  pala1.draw();
-
-  pala2.init(ctx);
-  pala2.draw();
-
-  var bola = new dobola();
-
-  bola.init(ctx);
-  bola.draw();
-
-
   var timer = null;
-  var start = document.getElementById('start');
 
-  //-- Comienza la animación!
+  var start  = document.getElementById('start');
+
   start.onclick = () => {
-    //-- Score selected:
-    var points = document.querySelector('input[name="points"]:checked').value;
-
-    if (!timer){
-      timer = setInterval(() =>{
-
-        //-- Actualizar elementos:
-        pala1.update();
-        pala2.update();
-        bola.update();
-        //-- Limpiamos el canvas:
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        //-- Dibujamos:
-        bola.draw();
-        pala1.draw();
-        pala2.draw();
-        score.draw();
-
-        moverPalas(pala1, pala2);
-        pushbola(pala1, pala2, bola);
-
-        if (bola.x > canvas.width - bola.ancho){
-          score.score1++;
-          //-- Saca Jugador2:
-          restartbola('Jugador1', bola, pala1, pala2);
-
-          bola.vx = -bola.vx;
-
-        } else if (bola.x < bola.ancho){
-          score.score2++;
-          //-- Saca Jugador1:
-          restartbola('Jugador2', bola, pala1, pala2);
-
-          bola.vx = -bola.vx;
-        }
-
-        //-- En función de la puntuación máxima:
-        if (score.score1 == points || score.score2 == points){
-
-          if (score.score1 == points){
-            alert("Player1 WINS - Congratulations!")
-          } else if (score.score2 == points){
-            alert("Player2 WINS - Congratulations!")
+    console.log("Click");
+    //-- Lanzar el timer (si es que no estaba ya lanzado)
+    if(!timer) {
+      timer = setInterval(() => {
+        //-- Actualizar elementos
+        racks.update()
+        palamaquina.update()
+        bola.update()
+        bola1.update()
+        bola2.update()
+        //-- Partir de un canvas limpio
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        //-- Dibujar los elementos
+        racks.draw()
+        palamaquina.draw()
+        bola.draw()
+        bola1.draw()
+        bola2.draw()
+        score.draw()
+        netdraw()
+        //--Condicion de terminacion
+        if(bola.x > canvas.width || bola.x < -10) {
+          if(bola.x > canvas.width){
+            //--Aumentar marcador 1
+            score.scor1 = score.scor1 + 1;
+          }else if (bola.x < -10) {
+            //--Aumentar marcador 2
+            score.scor2 = score.scor2 + 1;
           }
-
           clearInterval(timer);
           timer = null;
-          //-- Limpiamos el canvas:
-          ctx.clearRect(0,0,canvas.width, canvas.height);
-          //-- Reseteamos y dibujamos:
+          racks.reset();
+          palamaquina.reset();
           bola.reset();
-          pala1.reset();
-          pala2.reset();
-          score.reset();
-
           bola.draw();
-          pala1.draw();
-          pala2.draw();
-          score.draw();
         }
-      }, 20);
-    }
-  } //-- FIN ONCLICK;
-}
+        //-- Rebote
+        if(bola.y > canvas.height || bola.y < 0){
+          bola.vy = -bola.vy;
+        }else if(bola1.y > canvas.height || bola1.y < 0){
+            bola1.vy = -bola1.vy;
+        } else if(bola2.y > canvas.height || bola2.y < 0){
+            bola2.vy = -bola2.vy;
+        }
 
-  //-- ESCACIO: 32, FLECHA ARRIBA: 38, FLECHA ABAJO: 40
-  //-- Para consultar más: https://keycode.info
+        if(bola1.x > canvas.width || bola1.x < 0){
+           bola1.vx = -bola1.vx;
+        } else if(bola2.x > canvas.width || bola2.x < 0){
+             bola2.vx = -bola2.vx;
+        }
+        //-- Igualar rangos:
+        //codigo repetido
+        if (bola.x < 55){
+          ptop = racks.y1;
+          pbott = racks.y1 + racks.height;
+          pleft = racks.x1;
+          pright = racks.x1 + racks.width;
+          btop = bola.y;
+          bbott = bola.y + bola.height;
+          bleft = bola.x;
+          bright = bola.x + bola.width;
+          if (bright > pleft && btop < pbott && bleft < pright && bbott > ptop){
+            bola.vx = -bola.vx
+          }
+        }else if (bola.x > 445) {
+          ptop = racks.y2;
+          pbott = palamaquina.y2 + palamaquina.height;
+          pleft = palamaquina.x2;
+          pright = palamaquina.x2 + palamaquina.width;
+          btop = bola.y;
+          bbott = bola.y + bola.height;
+          bleft = bola.x;
+          bright = bola.x + bola.width;
+          if (bright > pleft && btop < pbott && bleft < pright && bbott > ptop){
+            bola.vx = -bola.vx
+          }
+        }
+      },20);
+    }
+  }
+}
